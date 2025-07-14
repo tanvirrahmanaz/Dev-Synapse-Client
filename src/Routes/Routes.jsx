60e695b7468eeb5c9e84d66cfea297741633e66e
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-07-15 02:21:48 +0600

    home screen loading issue fix

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,7 +2,7 @@
 
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 import Membership from "../pages/Membership";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
